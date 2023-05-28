62f11d64eb9abd9adde6f4e12c826677eb2dc468
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2023-05-28 11:55:14 -0600

    Final

--- Include/app.py
+++ Include/app.py
@@ -14,7 +14,7 @@ st.set_page_config(layout="wide")
 delitos = []
 lstCords = []
 
-with open("./resources/Delitos_Municipio.csv") as csv_file:
+with open("resources/Delitos_Municipio.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -27,6 +27,22 @@ with open("./resources/Delitos_Municipio.csv") as csv_file:
             Longitud=float(row[3])
             Latitud= float(row[4])
             delitos.append( [Delito,Municipio,Cantidad,Longitud,Latitud] )
+        line_count += 1
+    print(f'Processed {line_count} lines.')
+
+#Consumo de datos para el sidebar Top 10 Delitos
+topDelitos = []
+
+with open("resources/Reporte-Tipos-Delitos.csv", encoding="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+        else:
+            Delito=str(row[0])
+            Promedio=row[1]
+            topDelitos.append( [Delito,Promedio] )
         line_count += 1
     print(f'Processed {line_count} lines.')
 
@@ -69,10 +85,17 @@ if(selected == "Inicio"):
     redirect_url = "#safezoneconnect"
     st.markdown(f'<meta http-equiv="refresh" content="url={redirect_url}">', unsafe_allow_html=True)
 
-elif(selected == "Noticias"):
+if(selected == "Noticias"):
     # Redirigir automáticamente después de 5 segundos
-    redirect_url = "#descubre-lo-que-pasa-cerca-de-ti"
+    redirect_url = "#noticias-destacadas"
     st.markdown(f'<meta http-equiv="refresh" content="url={redirect_url}">', unsafe_allow_html=True)
+
+
+with st.sidebar:#Top Delitos
+    st.write("<h2>Top 10 - Los tipos de delitos más comunes en el estado</h2>", unsafe_allow_html=True)
+    for delito in topDelitos:
+        st.write(f"<h2>{delito[0]}</h2>", unsafe_allow_html=True)
+        st.write(delito[1])
 
 with st.container(): #Inicio
     st.write("""<h1 class="title-hero">SafeZoneConnect</h1>""", unsafe_allow_html=True)
@@ -95,7 +118,7 @@ with st.container(): #Inicio
         #st_lottie(lottie_coding, height=500, key="coding")
         st.image(image_security)
 with st.container(): #Mapa de calor
-    st.title("Mapas de Calor de Crimenes Del Estado de Nuevo Leon")
+    st.write("<h1>Mapas de Calor de Crimenes Del Estado de Nuevo Leon</h1>", unsafe_allow_html=True)
     for i in range( 0, len(delitos)-1 ):
         Map.getPoints(delitos[i][4], delitos[i][3], delitos[i][2], lstCords)
 
@@ -114,63 +137,47 @@ with st.container(): #Mapa de calor
 with st.container(): #Noticias
     st.write("---")
     # Título de la sección
-    st.title("Noticias Destacadas")
-    left_column, center_column, rigth_column = st.columns(3)
+    st.write("<h1>Noticias Destacadas</h1>", unsafe_allow_html=True)
 
     # Primera fila de cartas
     st.write("<div class='row'>", unsafe_allow_html=True)
-    with left_column:
+    with st.container():
         # Carta 1
         st.write("<div class='card'>", unsafe_allow_html=True)
         st.image("https://images.telediario.mx/J5sbQmzQShvLpSRe7BYak04pv6w=/958x596/uploads/media/2023/03/31/asesinan-balazo-cabeza-hombre-pelear.jpeg", width=300)
-        st.write("Título de la Noticia 1")
-        st.write("Contenido de la noticia 1")
+        st.write("<h2>Hombre dispara contra fachada de Secretaría de Seguridad en China, Nuevo León</h2>", unsafe_allow_html=True)
+        st.write(
+            """
+            Hombres armados dispararon contra la fachada del edificio de la Secretaría de Seguridad Pública del municipio de China, Nuevo León.
+            El ataque se reportó cerca de las 19:50 horas del martes, en el edificio ubicado sobre la calle Hidalgo. 
+            """)
         st.button("Leer más", "btnArt1")
         st.write("</div>", unsafe_allow_html=True)
-    with center_column:
+    with st.container():
         # Carta 2
         st.write("<div class='card'>", unsafe_allow_html=True)
-        st.image("https://images.telediario.mx/J5sbQmzQShvLpSRe7BYak04pv6w=/958x596/uploads/media/2023/03/31/asesinan-balazo-cabeza-hombre-pelear.jpeg", width=300)
-        st.write("Título de la Noticia 2")
-        st.write("Contenido de la noticia 2")
+        st.image("https://cdn2.excelsior.com.mx/media/styles/image800x600/public/pictures/2023/05/04/2944216.jpg", width=300)
+        st.write("<h2>Grupo armado roba 10 mdp a empleados de empresa de seguridad en Nuevo León</h2>", unsafe_allow_html=True)
+        st.write(
+            """
+            Un grupo armado despojó un total de 10 millones de pesos a empleados de una empresa de seguridad en el cruce de las calles Diego de Montemayor y Pedro Noriega, en la colonia Terminal del municipio de Monterrey, Nuevo León.
+            """)
         st.button("Leer más", "btnArt2")
         st.write("</div>", unsafe_allow_html=True)
-    with rigth_column:
+
+    with st.container():
         # Carta 3
         st.write("<div class='card'>", unsafe_allow_html=True)
-        st.image("https://images.telediario.mx/J5sbQmzQShvLpSRe7BYak04pv6w=/958x596/uploads/media/2023/03/31/asesinan-balazo-cabeza-hombre-pelear.jpeg", width=300)
-        st.write("Título de la Noticia 3")
-        st.write("Contenido de la noticia 3")
+        st.image("https://seguridad.sspc.gob.mx/uploads/portadas/thumbs/71c00570-4161-4927-8204-c83ac2552a1d-crop.jpg", width=300)
+        st.write("<h2>Guardia Nacional localiza 11 tomas clandestinas utilizadas para sustracción ilegal de hidrocarburo</h2>", unsafe_allow_html=True)
+        st.write(
+            """
+            En el marco del plan conjunto para combatir el robo de hidrocarburo, en los estados de Hidalgo y Nuevo León, elementos de la Guardia Nacional (GN) localizaron 11 tomas clandestinas, aparentemente utilizadas para la sustracción ilegal de combustible, y aseguraron un vehículo que contenía bidones cargados con aparente huachicol.
+            """)
         st.button("Leer más", "btnArt3")
         st.write("</div>", unsafe_allow_html=True)
     st.write("</div>", unsafe_allow_html=True)
 
-    # Segunda fila de cartas
-    st.write("<div class='row'>", unsafe_allow_html=True)
-    with left_column:
-        # Carta 4
-        st.write("<div class='card'>", unsafe_allow_html=True)
-        st.image("https://images.telediario.mx/J5sbQmzQShvLpSRe7BYak04pv6w=/958x596/uploads/media/2023/03/31/asesinan-balazo-cabeza-hombre-pelear.jpeg", width=300)
-        st.write("Título de la Noticia 4")
-        st.write("Contenido de la noticia 4")
-        st.button("Leer más", "btnArt4")
-        st.write("</div>", unsafe_allow_html=True)
-    with center_column:
-        # Carta 5
-        st.write("<div class='card'>", unsafe_allow_html=True)
-        st.image("https://images.telediario.mx/J5sbQmzQShvLpSRe7BYak04pv6w=/958x596/uploads/media/2023/03/31/asesinan-balazo-cabeza-hombre-pelear.jpeg", width=300)
-        st.write("Título de la Noticia 5")
-        st.write("Contenido de la noticia 5")
-        st.button("Leer más", "btnArt5")
-        st.write("</div>", unsafe_allow_html=True)
-    with rigth_column:
-        # Carta 6
-        st.write("<div class='card'>", unsafe_allow_html=True)
-        st.image("https://images.telediario.mx/J5sbQmzQShvLpSRe7BYak04pv6w=/958x596/uploads/media/2023/03/31/asesinan-balazo-cabeza-hombre-pelear.jpeg", width=300)
-        st.write("Título de la Noticia 6")
-        st.write("Contenido de la noticia 6")
-        st.button("Leer más", "btnArt6")
-        st.write("</div>", unsafe_allow_html=True)
-    st.write("</div>", unsafe_allow_html=True)
+    
 
     
